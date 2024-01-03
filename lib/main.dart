6c90dc4c4302503bf9ae7ade891089005a3e702e
JeinJang <jeinjang99@gmail.com>
2024-01-03 11:13:38 +0900

    👽️ api service 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/screens/webtoon/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
 class App extends StatelessWidget {
+  // List<WebtoonModel> webtoons = [];
+  // bool isLoading = true;
+
+  // void waitForWebToons() async {
+  //   webtoons = await ApiService.getTodaysToons();
+  //   isLoading = false;
+  //   setState(() {});
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   waitForWebToons();
+  // }
+
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/webtoon/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        foregroundColor: Colors.green,
+        title: const Text(
+          '오늘의 웹툰',
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // 최적화용 builder/separated
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+              // children: [
+              //   for (var webtoon in snapshot.data!) Text(webtoon.title),
+              // ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
